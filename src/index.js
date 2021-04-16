caeb2779b670e16c3788df6d2d7c58aca115625e
unknown <margarita.salazar.mls@gmail.com>
2021-04-16 12:45:25 -0500

    connected app with thung and logger middleware

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index'
+
+
 const { worker } = require('./mocks/browser');
 worker.start();
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
     rootElement
 );
 
